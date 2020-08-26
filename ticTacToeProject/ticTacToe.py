d1a66512a709107a508a4e2c792dbe885f3b77e9
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-08-26 18:27:36 -0500

    TODO: Build win condition function Add 2 Player option Think about any visual improvements Try exception method

--- ticTacToeProject/ticTacToe.py
+++ ticTacToeProject/ticTacToe.py
@@ -1,79 +1,87 @@
 
 import re
 import random
+# regular expression pattern for validation
 pattern = re.compile(r'[1-9]')
+# dictionary used for game printing and validation on computer/player turn
 game_square_dict = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9}
-num_list = [1,2,3,4,5,6,7,8,9]
-turn = {'turn':0}
+# used for turn tracking and winner announcement
+turn = {'turn':'player'}
+game_playing = True
+
 
 def main():
-    begin_game()
-    print_board()
-    user_turn()
+    # try:
+        begin_game()
+        print_board()
+        while game_playing:
+            user_turn()
+            computer_turn()
+    # except Exception as err:
+    #     print(err)
+
 
 # simple start for the user to see
 def begin_game():
     print('Welcome to Tic-Tac-Toe!')
     input('Press enter to begin!')
-    return
+
 
 # create and display game board
 def print_board():
     print('{:<1}{:^1}{:<2}{:<1}{:^1}{:<2}{:<1}{:^1}{:<1}'.format('_', f'{game_square_dict[1]}', '_|','_', f'{game_square_dict[2]}', '_|','_', f'{game_square_dict[3]}', '_'))
     print('{:<1}{:^1}{:<2}{:<1}{:^1}{:<2}{:<1}{:^1}{:<1}'.format('_', f'{game_square_dict[4]}', '_|','_', f'{game_square_dict[5]}', '_|','_', f'{game_square_dict[6]}', '_'))
-    print('{:<1}{:^1}{:<2}{:<1}{:^1}{:<2}{:<1}{:^1}{:<1}'.format(' ', f'{game_square_dict[7]}', ' |',' ', f'{game_square_dict[8]}', ' |',' ', f'{game_square_dict[9]}', ' '))
+    print('{:<1}{:^1}{:<2}{:<1}{:^1}{:<2}{:<1}{:^1}{:<1}'.format(' ', f'{game_square_dict[7]}', ' |',' ', f'{game_square_dict[8]}', ' |',' ', f'{game_square_dict[9]}', ' \n'))
 
-# ask user for entry during user turn, call for entry validation, call for win condition check, and call computer_turn method when finished
+
+# ask user for entry during user turn, call for entry validation, call for win condition check
 def user_turn():
     print('Player Turn!')
     num_selection = input('Choose an unoccupied square: ')
     square_choice = number_validation(num_selection)
     move_validation(square_choice)
-    return
 
-# runs computer turn and calls for win condition
+
 def computer_turn():
-    print("Computer Turn!")
-    num_selection = input(random.choice(num_list))
+    num_selection = random.randint(1,9)
     square_choice = number_validation(num_selection)
     move_validation(square_choice)
-    return
+
 
 def win_condition():
     return
 
-def number_validation(selection):
 
-    mo = pattern.search(selection)
-    while mo == None or len(selection) > 1:
+def number_validation(selection):
+    mo = pattern.search(str(selection))
+    length = len(str(selection))
+    while mo == None or length > 1:
         selection = input("Please enter a single number 1-9 with no spaces: ")
         mo = pattern.search(selection)
     regex = int(selection)
     return regex
 
+
 def move_validation(choice):
     mo = pattern.search(str(game_square_dict[choice]))
     whosUp = turn['turn']
-
     if mo == None:
-        if whosUp == 0:
+        if whosUp == 'player':
             print('Not allowed, Choose an empty square')
             print_board()
             user_turn()
-        elif whosUp == 1:
+        elif whosUp == 'computer':
             computer_turn()
     else:
-        if whosUp == 0:
-            turn['turn'] = 1
+        if whosUp == 'player':
+            turn['turn'] = 'computer'
             game_square_dict[choice] = 'X'
             print_board()
-            computer_turn()
-        else:
+            print("Computer Turn!")
+        elif whosUp == 'computer':
             game_square_dict[choice] = 'O'
-            turn['turn'] = 0
+            turn['turn'] = 'player'
             print_board()
-            user_turn()
 
-    return
 
 main()
